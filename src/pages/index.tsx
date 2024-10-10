5555b9ed31e8a6b23c55f06f1be2f0c928ebab28
Nikita Krachko <nikikra@mail.ru>
2024-10-10 14:46:03 +0300

    feat: add pages

---
+++ src/pages/index.tsx
@@ -0,0 +1,24 @@
+import React, { FC } from 'react'
+
+import { Container, Typography } from '@mui/material'
+
+import { TaskProvider, ThemeProvider } from '@/app/providers'
+import { TaskForm, TaskList } from '@/features/tasks/ui'
+
+const Home: FC = () => {
+  return (
+    <ThemeProvider>
+      <TaskProvider>
+        <Container>
+          <Typography gutterBottom variant='h1'>
+            To-Do List
+          </Typography>
+          <TaskForm />
+          <TaskList />
+        </Container>
+      </TaskProvider>
+    </ThemeProvider>
+  )
+}
+
+export default Home
